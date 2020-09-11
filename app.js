849930abeaebd8667b3c7c8bf85c1b4f5a2ea390
mikelcabor <39592891+mikelcabor@users.noreply.github.com>
2020-09-11 17:54:17 +0200

    gooder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
-app.listen(3000, () => console.log(`listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
 app.use(express.static('public'));
 
