05b216f9dc70a0cfd5e2a3cc8ce5b2cc08f0b4f7
mikelcabor <39592891+mikelcabor@users.noreply.github.com>
2020-09-11 20:17:40 +0200

    post and json write

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const router = express.Router();
+const bodyParser = require('body-parser');
+var fs = require('fs');
+const { callbackify } = require('util');
 
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+
+//all that is in here is supose to be public
 app.use(express.static('public'));
 
+//body parser support parsing of application/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.get('/forms', (req, res) => {
+    console.log("forms");
+    res.sendFile('public/forms.html', { root: __dirname });
+});
+
+
+app.post('/hola', function (req, res) {
+    console.log('post');    
+    console.log(req.body);
+    const data = JSON.stringify(req.body);
+    fs.writeFile('code.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    });
+    res.redirect('/');
+  });
+
+  app.get('/leer',(req, res) => {
+        
+    //borrar    
+    fs.readFile('code.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+    
+        // parse JSON object
+        const user = JSON.parse(data.toString());
+    
+        // print JSON object
+        console.log(user.textarea);
+    });
+  });
+ 
--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 
 //Submit del boton en forms.html
 console.log('Client-side code running');
-let codes = ['hello'];
+
 
 function addNewCode() {
     console.log("hello")
-    var codeString = JSON.stringify(codes); 
-    console.log(codeString);
+    //var codeString = JSON.stringify(codes); 
+    //console.log(codeString);
   }
  
