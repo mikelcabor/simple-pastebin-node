766c3c5f2446b1612cc644f2941af9afcbf2ec7a
mikelcabor <39592891+mikelcabor@users.noreply.github.com>
2020-09-12 17:31:24 +0200

    index shows code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000
 const router = express.Router();
 const bodyParser = require('body-parser');
 var fs = require('fs');
-const { json } = require('body-parser');
 var currentCode = [];
 var newCode = [];
 
@@ -18,6 +17,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//route to user
+router.use('/index', (req, res) => {
+    console.log("index");
+    res.sendFile('public/index.html', { root: __dirname });
+});
+
+app.use('/', router);
+
 
 app.get('/forms', (req, res) => {
     console.log("forms");
@@ -25,7 +32,7 @@ app.get('/forms', (req, res) => {
 });
 
 
-app.post('/hola', function (req, res) {
+app.post('/addCode', function (req, res) {
     console.log('post');        
 
     newCode.push(req.body);
@@ -41,36 +48,10 @@ app.post('/hola', function (req, res) {
         fs.writeFile('code.json', json, err => { if (err) console.log(err) }); // write it back 
     }});
 
-
-
-
-    /*fs.readFile('code.json', (err, data) => {
-        if (err) {
-            throw err;
-        }    
-        // parse JSON object
-        const readCode = JSON.parse(data.toString());
-    
-        // print JSON object
-        console.log(readCode);
-    });
-    let rawdata = fs.readFileSync('code.json');
-    let code = JSON.parse(rawdata);
-    code.push(...anchors);
-
-    console.log(student);
-    const data = JSON.stringify(req.body);
-    fs.writeFile('code.json', student, data, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON data is saved.");
-    });*/
     res.redirect('/');
   });
 
-  app.get('/leer',(req, res) => {
-        
+  app.get('/leer',(req, res) => {        
     //borrar    
     fs.readFile('code.json', 'utf-8', (err, data) => {
         if (err) {
@@ -78,10 +59,13 @@ app.post('/hola', function (req, res) {
         }
     
         // parse JSON object
-        const readCode = JSON.parse(data.toString());
-    
+        //var readCode = JSON.parse(data.toString());
+        
         // print JSON object
-        console.log(readCode);
+        
+        res.end(data);
+        
+        
     });
   });
  
--- public/script.js
+++ public/script.js
@@ -1,6 +1,68 @@
 
 //Submit del boton en forms.html
-console.log('Client-side code running');
+
+console.log(window.location.hostname);
+const path = window.location.hostname + "/leer";
+var pageElement;
+
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+} 
+
+var json_obj = JSON.parse(Get('http://localhost:3000/leer'));
+
+window.onload = function (){
+    for (var i = json_obj.length - 1; i >= 0; i--) {
+        console.log(json_obj[i]);
+        
+        const div = document.createElement("div");
+        div.className = "container";
+        div.innerHTML = "<div class='row'>"+
+            "<div class='col m12'>"+
+                "<div class='card darken-1'>" +
+                    "<div class='card-content'>" +
+                        "<span class='card-title'>"+ json_obj[i].name +"</span>" +
+                        "<pre>" + json_obj[i].textarea +"</pre>" +
+                        "</div>" +
+                        "<div class='card-action'>"+
+                        "<a href='#'>Copiar codigo</a>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>";
+        
+         const container = document.getElementById("container");
+         container.appendChild(div);
+    }
+    /*json_obj.forEach(element => {
+        console.log(element);   
+        const div = document.createElement("div");
+        div.className = "container";
+        div.innerHTML = "<div class='row'>"+
+            "<div class='col m12'>"+
+                "<div class='card darken-1'>" +
+                    "<div class='card-content'>" +
+                        "<span class='card-title'>"+ element.name +"</span>" +
+                        "<pre>" + element.textarea +"</pre>" +
+                        "</div>" +
+                        "<div class='card-action'>"+
+                        "<a href='#'>Copiar codigo</a>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>";
+        
+         const container = document.getElementById("container");
+         container.appendChild(div);
+              
+    });*/
+}
+
+
+
+
+
 
 
  
